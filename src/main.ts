e0de87258eb13061da66540ce0ba46ce952d9995
SaadMeddiche <saadmeddiche2004201@gmail.com>
2025-04-21 14:56:00 +0100

    feat: update main.ts to use CircleGenerator for circle creation

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,9 @@
-import {canvas} from "./constants/MainConstants.ts";
-import {Circle} from "./models/Circle.ts";
 import {start as startCircleAnimation} from "./scripts/CircleAnimator.ts";
+import {CircleGenerator} from "./scripts/CircleGenerator.ts";
 
-const blueCircle :Circle = new Circle('blue', 20, canvas.width * Math.random(), canvas.height * Math.random(), 2, 2);
-const redCircle :Circle = new Circle('red', 20, canvas.width * Math.random(), canvas.height * Math.random(), 2, 2);
-const greenCircle :Circle = new Circle('green', 20, canvas.width * Math.random(), canvas.height * Math.random(), 2, 2);
+const circles = CircleGenerator .generateCircles(10)
 
-startCircleAnimation([blueCircle, redCircle, greenCircle]);
+startCircleAnimation(circles);
 
 
 
